ebea0ebc2a5574fa00e520909ae38f4a56052efc
peder august winge <54266739+square-max@users.noreply.github.com>
2019-11-27 10:51:51 +0100

    Updates

---
+++ precache-manifest.94dff79b623b29f743ecf5b7e02a7ee9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a95d1022e05f198877d62cccf4867c3",
+    "url": "/react-submodules/index.html"
+  },
+  {
+    "revision": "e0c27fba72a40d1d247d",
+    "url": "/react-submodules/static/css/main.1ceafde4.chunk.css"
+  },
+  {
+    "revision": "da8af002cc14d55d6177",
+    "url": "/react-submodules/static/js/2.a922cbb3.chunk.js"
+  },
+  {
+    "revision": "e0c27fba72a40d1d247d",
+    "url": "/react-submodules/static/js/main.8598b76e.chunk.js"
+  },
+  {
+    "revision": "c900d3900270f3cc22a9",
+    "url": "/react-submodules/static/js/runtime-main.6779e2cf.js"
+  }
+]);
