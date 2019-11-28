fc542279a8649787f04cc024cbe54cd6e5decfe0
peder august winge <54266739+square-max@users.noreply.github.com>
2019-11-28 14:46:50 +0100

    ok

--- src/components/Character.js
+++ src/components/Character.js
@@ -2,17 +2,17 @@ import React from 'react'
 import './Character.css'
 
 const Character = (props) => {
+console.log(props)
     return (
         <div className="character">
-<img src={props.img} alt={"The characters: " + props.name} />
-<h3>{props.name}</h3>
-<hr />
-<p>
-    {props.gender} <br />
-    {props.status} <br />
-    {props.origin} <br />
-
-</p>
+        <img src={props.img} alt={"The characters: " + props.name} />
+        <h3>{props.name}</h3>
+        <hr />
+        <p>
+            {props.gender} <br />
+            {props.status} <br />
+            {props.origin} <br />
+        </p>
         </div>
     )
 }
